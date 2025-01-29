8da0657d79ae566b6b7c9e7bde9da8ad8b668db3
Akash M Nandan <akash003nandan@gmail.com>
2025-01-30 01:02:13 +0530

    feat: categoriesListingScreen file created

---
+++ screens/CategoriesListingScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CategoriesListingScreen() {
+  return (
+    <View>
+      <Text>CategoriesListingScreen</Text>
+    </View>
+  )
+}
